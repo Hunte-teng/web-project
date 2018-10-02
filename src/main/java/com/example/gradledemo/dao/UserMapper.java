f98624fc926b99bba696a129d865ec178fdb5dcb
Hunte-teng <1427473112@qq.com>
2018-10-02 17:34:33 +0800

    v_001

--- src/main/java/com/example/gradledemo/dao/UserMapper.java
+++ src/main/java/com/example/gradledemo/dao/UserMapper.java
@@ -1,6 +1,8 @@
 package com.example.gradledemo.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
