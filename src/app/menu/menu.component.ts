3d27da76f3804d810c729e60ec04a5a41a61fcd9
Hunte-teng <1427473112@qq.com>
2019-07-07 22:24:00 +0800

    first commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  // template: `
+  //   <p>
+  //     menu works!
+  //   </p>
+  // `,
+  templateUrl: './menu.component.html',
+  // styles: [],
+  styleUrls :['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
